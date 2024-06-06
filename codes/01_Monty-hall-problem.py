bccc1099feea6da786a94f9eec417c7d0b035f79
Amirhossein Heydari <amirhosseinheydari78@gmail.com>
2024-06-06 18:30:21 +0330

    Update Monty-Hall Game

--- codes/01_Monty-hall-problem.py
+++ codes/01_Monty-hall-problem.py
@@ -1,39 +1,51 @@
-# more details about "Monty Hall problem": https://en.wikipedia.org/wiki/Monty_Hall_problem
+# Monty Hall problem: https://en.wikipedia.org/wiki/Monty_Hall_problem
 
 # dependencies
 import random
+import matplotlib.pyplot as plt
 
-# monti-hall simulation
-def monty_hall_simulation(num_simulations: int) -> tuple[float]:
-    switch_wins = stay_wins = 0
 
-    for _ in range(num_simulations):
-        doors = ['car', 'goat', 'goat']
+# initial objects behind each door
+doors = ['car', 'goat', 'goat']
+
+# create a figure to plot a bar chart
+fig, ax = plt.subplots()
+plt.suptitle('Monty-Hall Game')
+
+
+def monty_hall_simulation(num_simulations: int) -> None:
+
+    # frequency for winning for each player
+    player_1_wins = 0  # the switch guy
+    player_2_wins = 0  # the stay guy
+
+    for i in range(1, num_simulations + 1):
 
         # shuffle the doors
         random.shuffle(doors)
 
-        # player's first choice
-        first_choice = random.choice(doors)
+        # choosing a door: [0, 1, 2]
+        player_1 = random.randint(0, 2)
+        player_2 = random.randint(0, 2)
 
-        # switch the door
-        if first_choice == 'goat':
-            switch_wins += 1
+        # player 1: if the chosen door is 'goat', switching to another door is a win
+        if doors[player_1] == 'goat':
+            player_1_wins += 1
 
-        # do not switch the door
-        elif first_choice == 'car':
-            stay_wins += 1
+        # player 2: if the chosen door is 'car', staying with the door is a win
+        if doors[player_2] == 'car':
+            player_2_wins += 1
 
-    # calculate probabilities
-    switch_win_prob = (switch_wins / num_simulations) * 100
-    stay_win_prob   = (stay_wins   / num_simulations) * 100
+        # plot
+        ax.clear()
+        ax.set_title(f"#Game: {i} | switch win ratio: {player_1_wins / i:2.2f} | stay win ratio: {player_2_wins / i:2.2f}")
+        ax.bar(['Player 1', 'Player 2'], [player_1_wins / i, player_2_wins / i], label=['wins for player 1', 'wins for player 2'], color=['red', 'blue'])
+        ax.set_yticks([0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
+        ax.legend()
+        plt.pause(0.03)
 
-    return (switch_win_prob, stay_win_prob)
 
 if __name__ == '__main__':
     num_simulations = 100000
-    switch_win_prob, stay_win_prob = monty_hall_simulation(num_simulations)
-
-    print(f"number of simulations: {num_simulations} times")
-    print(f"probability of winning by     switching doors : {switch_win_prob:.2f} %")
-    print(f"probability of winning by not switching doors : {stay_win_prob:.2f} %")
+    monty_hall_simulation(num_simulations)
+    plt.show()
